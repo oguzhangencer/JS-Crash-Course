c9292709eb36a497b101a274fbd34b3f5599c473
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-08-02 20:52:00 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+// Template Literals
+
+const name = 'Oguz'
+
+const message = `Hello ${name}`
+
+console.log(message)
+
+const unit_price = 100
+
+const piece = 3
+
+const currency = "TL"
+
+const total_amount = `Total: ${unit_price * piece} ${currency}`
+
+console.log(total_amount)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
