3d8a084a356f6d05d7d4c03bc3a3ca4b9850aa06
Oğuzhan Gök <79082594+oguzhangencer@users.noreply.github.com>
2022-08-02 21:15:25 +0300

    arrow_function

---
+++ index3.js
@@ -0,0 +1,17 @@
+// arrow function
+
+console.log(sum(2, 4)) //  it works
+
+function sum (a, b){
+  return a + b
+}
+
+//
+//
+//
+
+console.log(power(2, 3)) // it does not work
+
+const power = (a, b) => a ** b
+
+console.log(power(2, 3)) // it works
